ca41f71677c574750697508dbde94d2cdb6322c3
Alexander <Alexander_alatorre1993@hotmail.com>
2021-12-06 20:38:01 -0800

    added new files

--- Types/generics.ts
+++ Types/generics.ts
@@ -1,5 +1,5 @@
 // you can replace the string with an any Type so replace string w/ T
-
+// every place we use T becomes the type we pass to it
 function simpleTState<T>(initial: T): [() => T, (v: T) => void] {
   let str: T = initial;
 
@@ -10,3 +10,48 @@ function simpleTState<T>(initial: T): [() => T, (v: T) => void] {
     },
   ];
 }
+
+const [stateGetter, stateSetter] = simpleTState(1);
+console.log(stateGetter());
+
+const [state1Getter, state2Setter] = simpleTState("alex");
+console.log(state1Getter());
+
+// <string | null> overwrites the values
+const [state2Getter, state3Setter] = simpleTState<string | null>(null);
+console.log(state2Getter());
+state3Setter("ssdfa");
+console.log(state2Getter());
+
+// ranker
+// if you don't have access to RankItem outside just add a generic Rank<RankItem>
+
+interface Rank<RankItem> {
+  item: RankItem;
+  rank: number;
+}
+[];
+
+function ranker<RankItem>(items: RankItem[], rank: (v: RankItem) => number) {
+  const ranks: Rank<RankItem>[] = items.map((item) => ({
+    item,
+    rank: rank(item),
+  }));
+
+  ranks.sort((a, b) => a.rank - b.rank);
+
+  return ranks.map((rank) => rank.item);
+}
+
+interface Pokemon {
+  name: string;
+  hp: number;
+}
+
+const pokemon: Pokemon[] = [
+  { name: "Boulvasor", hp: 20 },
+  { name: "Charizard", hp: 23 },
+];
+
+const ranks = ranker(pokemon, ({ hp }) => hp);
+console.log(ranks)
